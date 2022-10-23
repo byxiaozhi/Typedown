08248b210eadb4ca8d294f6077e3db5ac6976cb0
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-10-24 02:27:26 +0800

    Fix project packaging issues

--- Typedown/Windows/MainWindow.xaml.cs
+++ Typedown/Windows/MainWindow.xaml.cs
@@ -13,11 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Typedown
+namespace Typedown.Windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
