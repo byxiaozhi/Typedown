1c93ff4e4e72e9efb9789a82b9e97ef2cfad3690
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-10-24 01:32:13 +0800

    Create empty project

---
+++ Typedown/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typedown
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (new Universal.App())
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
