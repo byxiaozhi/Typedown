d1cbdd7faaf0ebedb74e3ac8d54af3d455fc57c3
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-11-06 15:04:59 +0800

    Add caption animation

--- Typedown.Universal/Controls/CaptionControls/Caption.xaml.cs
+++ Typedown.Universal/Controls/CaptionControls/Caption.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Typedown.Universal.Utilities;
 using Typedown.Universal.ViewModels;
 using Windows.UI.Xaml;
@@ -12,11 +13,32 @@ namespace Typedown.Universal.Controls
     {
         public AppViewModel ViewModel => DataContext as AppViewModel;
 
+        private readonly CompositeDisposable disposables = new();
+
         public Caption()
         {
             InitializeComponent();
             if (Config.IsMicaSupported)
                 CaptionWrapper.Children.Remove(CaptionWrapper.Children.OfType<CaptionButtons>().First());
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            disposables.Add(ViewModel.GoBackCommand
+                .WhenPropertyChanged(nameof(ViewModel.GoBackCommand.IsExecutable))
+                .Cast<bool>()
+                .Subscribe(x => UpdateBackButtonState(x)));
+            UpdateBackButtonState(ViewModel.GoBackCommand.IsExecutable, false);
+        }
+
+        private void UpdateBackButtonState(bool canGoBack, bool useTransitions = true)
+        {
+            VisualStateManager.GoToState(this, canGoBack ? "BackVisible" : "BackCollapsed", useTransitions);
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            disposables.Clear();
         }
     }
 }
