10ad7617c46fa9febf1fd7fd5cc1281477765dc3
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-11-07 11:45:18 +0800

    Optimize flyout

---
+++ Typedown.Universal/Controls/FloatControls/TableTools.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Typedown.Universal.Interfaces;
+using Typedown.Universal.ViewModels;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Typedown.Universal.Controls.FloatControls
+{
+    public sealed partial class TableTools : MenuFlyout
+    {
+        public bool IsRow { get; private set; }
+
+        public Visibility RowItemVisibility => IsRow ? Visibility.Visible : Visibility.Collapsed;
+
+        public Visibility ColumnItemVisibility => !IsRow ? Visibility.Visible : Visibility.Collapsed;
+
+        private readonly IMarkdownEditor markdownEditor;
+
+        public TableTools(SettingsViewModel settings, IMarkdownEditor markdownEditor)
+        {
+            this.markdownEditor = markdownEditor;
+            AreOpenCloseAnimationsEnabled = settings.AnimationEnable;
+            InitializeComponent();
+        }
+
+        public void Open(Rect rect, string type)
+        {
+            IsRow = type != "bottom";
+            OverlayInputPassThroughElement = (markdownEditor as UIElement).XamlRoot.Content;
+            UpdateItemVisibility();
+            ShowAt(markdownEditor.GetDummyRectangle(rect));
+        }
+
+        private void UpdateItemVisibility()
+        {
+            InsertPreviousRow.Visibility = InsertNextRow.Visibility = RemoveNurrentRow.Visibility = RowItemVisibility;
+            InsertLeftColumn.Visibility = InsertRightColumn.Visibility = RemoveCurrentColumn.Visibility = ColumnItemVisibility;
+        }
+
+        private void InsertPreviousRow_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "insert", location = "previous", target = "row" });
+        }
+
+        private void InsertNextRow_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "insert", location = "next", target = "row" });
+        }
+
+        private void RemoveNurrentRow_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "remove", location = "current", target = "row" });
+        }
+
+        private void InsertLeftColumn_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "insert", location = "left", target = "column" });
+        }
+
+        private void InsertRightColumn_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "insert", location = "right", target = "column" });
+        }
+
+        private void RemoveCurrentColumn_Click(object sender, RoutedEventArgs e)
+        {
+            PostEditTableMessage(new { action = "remove", location = "current", target = "column" });
+        }
+
+        private void PostEditTableMessage(object args)
+        {
+            markdownEditor.PostMessage("EditTable", args);
+        }
+    }
+}
--- Typedown.Universal/Controls/FloatControls/ToolTip.xaml.cs
+++ Typedown.Universal/Controls/FloatControls/ToolTip.xaml.cs
@@ -1,22 +1,10 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Web;
+﻿using System;
 using Typedown.Universal.Interfaces;
 using Typedown.Universal.ViewModels;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace Typedown.Universal.Controls.FloatControls
 {
