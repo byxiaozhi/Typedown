10235e4bb92bff91bb66133c3eb1352df3d027eb
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2023-02-27 15:20:21 +0800

    Test Generate Translation Resources

---
+++ Tools/TranslationTool/BingTranslate.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace TranslationTool
+{
+    public class BingTranslate
+    {
+        private static readonly string key = "4afab82abb7a4871a6f0c8083b522f39";
+
+        private static readonly string endpoint = "https://api.cognitive.microsofttranslator.com/";
+
+        public static async Task<List<Dictionary<string, string>>> Translate(string from, List<string> to, List<string> text)
+        {
+            var route = $"/translate?api-version=3.0&from={from}&{string.Join('&', to.Select(x => $"to={x}"))}";
+            var body = text.Select(t => new { Text = t }).ToArray();
+            var requestBody = JsonConvert.SerializeObject(body);
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri(endpoint + route);
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+            request.Headers.Add("Ocp-Apim-Subscription-Region", "eastasia");
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JArray.Parse(content);
+            return result.Select(x => x["translations"].ToDictionary(x => x["to"].ToString(), x => x["text"].ToString())).ToList();
+        }
+
+        public static async Task<Dictionary<string, string>> GetSupportedLangs()
+        {
+            var route = $"/languages?api-version=3.0";
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri(endpoint + route);
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JObject.Parse(content)["translation"].ToObject<Dictionary<string, JObject>>().ToDictionary(x => x.Key, x => x.Value["nativeName"].ToString());
+            return result;
+        }
+    }
+}
--- Tools/TranslationTool/Program.cs
+++ Tools/TranslationTool/Program.cs
@@ -1,16 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TranslationTool;
+﻿using TranslationTool;
 
-var inputs = TextResource.ReadItems(@"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
-var dictionary = TextDictionary.ReadItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\").ToList();
+var zhInputs = TextResource.ReadItems(@"C:\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
+var dictionary = TextDictionary.ReadItems(@"C:\GitHub\Typedown\Tools\TranslationTool\Dictionary\").ToList();
 
 foreach (var lang in TextDictionary.SupportedLangs.Keys)
 {
-    var output = inputs.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, lang)).Where(x => !string.IsNullOrEmpty(x.Value));
-    output.WriteItems(@$"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\{lang}\");
+    var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).ToList();
+    if (!inputs.Any())
+        continue;
+    var results = await BingTranslate.Translate("zh-Hans", new List<string> { lang }, inputs.Select(x => zhInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList());
+    for (var i = 0; i < inputs.Count; i++)
+    {
+        foreach (var resultDic in results[i])
+        {
+            inputs[i].Values[resultDic.Key] = resultDic.Value;
+        }
+    }
+    dictionary.WriteItems(@"C:\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
+    Console.WriteLine(lang);
 }
 
-// dictionary = dictionary.Merge(inputs, "zh-Hans").ToList();
-// dictionary.WriteItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
+foreach (var lang in TextDictionary.SupportedLangs.Keys)
+{
+    var output = dictionary.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, lang)).Where(x => !string.IsNullOrEmpty(x.Value));
+    output.WriteItems(@$"C:\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\{lang}\");
+}
 
 Console.WriteLine("完成");
