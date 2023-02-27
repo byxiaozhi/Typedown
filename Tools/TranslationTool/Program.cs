c8434d7cfae9c80de60dbb36e802a6526c7c3a9f
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2023-02-28 00:29:59 +0800

    Complete english translation

--- Tools/TranslationTool/Program.cs
+++ Tools/TranslationTool/Program.cs
@@ -1,30 +1,65 @@
 ﻿using TranslationTool;
 
-var zhInputs = TextResource.ReadItems(@"C:\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
-var dictionary = TextDictionary.ReadItems(@"C:\GitHub\Typedown\Tools\TranslationTool\Dictionary\").ToList();
+var zhInputs = TextResource.ReadItems(@"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
+var enInputs = TextResource.ReadItems(@"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\en\").ToList();
+var dictionary = TextDictionary.ReadItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\").ToList();
 dictionary = dictionary.Merge(zhInputs, "zh-Hans").ToList();
+dictionary = dictionary.Merge(enInputs, "en").ToList();
+dictionary.WriteItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
 
-foreach (var lang in TextDictionary.SupportedLangs.Keys)
+foreach (var lang in TextDictionary.SupportedLangs.Keys.Where(x => x == "en"))
 {
-    var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).ToList();
-    if (!inputs.Any())
-        continue;
-    var results = await BingTranslate.Translate("zh-Hans", new List<string> { lang }, inputs.Select(x => zhInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList());
-    for (var i = 0; i < inputs.Count; i++)
+    while (true)
     {
-        foreach (var resultDic in results[i])
-        {
-            inputs[i].Values[resultDic.Key] = resultDic.Value;
-        }
+        var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).Take(30).ToList();
+        if (!inputs.Any())
+            break;
+
+        Console.WriteLine($"源语言：zh-Hans({TextDictionary.SupportedLangs["zh-Hans"]})");
+        Console.WriteLine($"目标语言：{lang}({TextDictionary.SupportedLangs[lang]})");
+
+        var words = inputs.Select(x => zhInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList();
+        Console.WriteLine($"待翻译文本(逗号分割)：" + string.Join(",", words));
+
+        Console.WriteLine($"翻译结果(逗号分割)：");
+        var results = Console.ReadLine().Split(",").ToList();
+
+        if (words.Count != results.Count)
+            continue;
+
+        for (var i = 0; i < inputs.Count; i++)
+            inputs[i].Values[lang] = results[i].Trim();
+
+        dictionary.WriteItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
+
+        Console.WriteLine();
     }
-    dictionary.WriteItems(@"C:\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
-    Console.WriteLine(lang);
-}
 
-foreach (var lang in TextDictionary.SupportedLangs.Keys)
-{
     var output = dictionary.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, lang)).Where(x => !string.IsNullOrEmpty(x.Value));
-    output.WriteItems(@$"C:\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\{lang}\");
+    output.WriteItems(@$"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\{lang}\");
 }
 
-Console.WriteLine("完成");
+//foreach (var lang in TextDictionary.SupportedLangs.Keys)
+//{
+//    var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).ToList();
+//    if (!inputs.Any())
+//        continue;
+//    var results = await BingTranslate.Translate("zh-Hans", new List<string> { lang }, inputs.Select(x => zhInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList());
+//    for (var i = 0; i < inputs.Count; i++)
+//    {
+//        foreach (var resultDic in results[i])
+//        {
+//            inputs[i].Values[resultDic.Key] = resultDic.Value;
+//        }
+//    }
+//    dictionary.WriteItems(@"C:\Users\12283\Documents\GitHub\Typedown\Tools\TranslationTool\Dictionary\");
+//    Console.WriteLine(lang);
+//}
+
+//foreach (var lang in TextDictionary.SupportedLangs.Keys)
+//{
+//    var output = dictionary.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, lang)).Where(x => !string.IsNullOrEmpty(x.Value));
+//    output.WriteItems(@$"C:\Users\12283\Documents\GitHub\Typedown\Dev\Typedown.Core\Resources\Strings\{lang}\");
+//}
+
+//Console.WriteLine("完成");
