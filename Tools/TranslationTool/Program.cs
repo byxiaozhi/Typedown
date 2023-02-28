141c460a4423baa169306488e4208ca53064d2be
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2023-02-28 14:14:28 +0800

    Complete Arabic translation

--- Tools/TranslationTool/Program.cs
+++ Tools/TranslationTool/Program.cs
@@ -6,17 +6,22 @@ Console.InputEncoding = Encoding.Unicode;
 
 string projectPath = @"C:\GitHub\Typedown";
 
-var zhInputs = TextResource.ReadItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
-var enInputs = TextResource.ReadItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\en\").ToList();
-var dictionary = TextDictionary.ReadItems(@$"{projectPath}\Tools\TranslationTool\Dictionary\").ToList();
-dictionary = dictionary.Merge(zhInputs, "zh-Hans").ToList();
-dictionary = dictionary.Merge(enInputs, "en").ToList();
-dictionary.WriteItems(@$"{projectPath}\Tools\TranslationTool\Dictionary\");
-
 var manualLangs = new List<string>() { "en", "ja", "ko", "ru", "fr", "de", "es", "it", "nl", "ar" };
 var batch = 30;
+
 foreach (var lang in manualLangs)
 {
+    ManualTranslate(lang);
+}
+
+void ManualTranslate(string lang)
+{
+    var zhInputs = TextResource.ReadItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\zh-Hans\").ToList();
+    var enInputs = TextResource.ReadItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\en\").ToList();
+    var dictionary = TextDictionary.ReadItems(@$"{projectPath}\Tools\TranslationTool\Dictionary\").ToList();
+    dictionary = dictionary.Merge(zhInputs, "zh-Hans").ToList();
+    dictionary = dictionary.Merge(enInputs, "en").ToList();
+    dictionary.WriteItems(@$"{projectPath}\Tools\TranslationTool\Dictionary\");
     while (true)
     {
         var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).Take(batch++).ToList();
@@ -52,27 +57,19 @@ foreach (var lang in manualLangs)
     output.WriteItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\{lang}\");
 }
 
-//foreach (var lang in TextDictionary.SupportedLangs.Keys)
-//{
-//    var inputs = dictionary.Where(x => !x.Values.ContainsKey(lang) || string.IsNullOrEmpty(x.Values[lang])).ToList();
-//    if (!inputs.Any())
-//        continue;
-//    var results = await BingTranslate.Translate("zh-Hans", new List<string> { lang }, inputs.Select(x => zhInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList());
-//    for (var i = 0; i < inputs.Count; i++)
-//    {
-//        foreach (var resultDic in results[i])
-//        {
-//            inputs[i].Values[resultDic.Key] = resultDic.Value;
-//        }
-//    }
-//    dictionary.WriteItems(@$"{projectPath}\Tools\TranslationTool\TempDictionary\");
-//    Console.WriteLine(lang);
-//}
-
-//foreach (var lang in TextDictionary.SupportedLangs.Keys)
-//{
-//    var output = dictionary.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, lang)).Where(x => !string.IsNullOrEmpty(x.Value));
-//    output.WriteItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\{lang}\");
-//}
-
-//Console.WriteLine("完成");
+async Task AutoTranslate(string sourceLang, string targetLang)
+{
+    var sourceInputs = TextResource.ReadItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\{sourceLang}\").ToList();
+    var dictionary = TextDictionary.ReadItems(@$"{projectPath}\Tools\TranslationTool\BingDictionary\").ToList();
+    dictionary = dictionary.Merge(sourceInputs, sourceLang).ToList();
+    var inputs = dictionary.Where(x => !x.Values.ContainsKey(targetLang) || string.IsNullOrEmpty(x.Values[targetLang])).ToList();
+    if (!inputs.Any())
+        return;
+    var results = await BingTranslate.Translate(sourceLang, new List<string> { targetLang }, inputs.Select(x => sourceInputs.Single(y => (x.Table, x.Name) == (y.Table, y.Name))).Select(x => x.Value).ToList());
+    for (var i = 0; i < inputs.Count; i++)
+        foreach (var resultDic in results[i])
+            inputs[i].Values[resultDic.Key] = resultDic.Value;
+    dictionary.WriteItems(@$"{projectPath}\Tools\TranslationTool\TempDictionary\");
+    var output = dictionary.Select(x => dictionary.GetTextResourceItem(x.Table, x.Name, targetLang)).Where(x => !string.IsNullOrEmpty(x.Value));
+    output.WriteItems(@$"{projectPath}\Dev\Typedown.Core\Resources\Strings\{targetLang}\");
+}
