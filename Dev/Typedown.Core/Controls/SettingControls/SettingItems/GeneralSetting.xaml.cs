10289e3709f83026074867060dc687e230c40383
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2023-02-28 12:55:51 +0800

    Add language switching restart tip

--- Dev/Typedown.Core/Controls/SettingControls/SettingItems/GeneralSetting.xaml.cs
+++ Dev/Typedown.Core/Controls/SettingControls/SettingItems/GeneralSetting.xaml.cs
@@ -8,6 +8,7 @@ using Typedown.Core.Utilities;
 using Typedown.Core.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Globalization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,6 +33,20 @@ namespace Typedown.Core.Controls.SettingControls.SettingItems
         public static bool IsStartupOpenFolderItemLoad(FolderStartupAction action)
         {
             return action == FolderStartupAction.OpenFolder;
+        }
+
+        private bool IsLangChanged(string settingLang)
+        {
+            try
+            {
+                var settingLanguage = Settings.Language;
+                var currentLanguage = ApplicationLanguages.PrimaryLanguageOverride;
+                return Locale.SupportedLangs.ContainsKey(settingLanguage) != Locale.SupportedLangs.ContainsKey(currentLanguage) || (Locale.SupportedLangs.ContainsKey(settingLanguage) && settingLanguage != currentLanguage);
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
