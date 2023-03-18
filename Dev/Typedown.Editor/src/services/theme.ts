ee079d9b7167bee1a3a86e3903a4c5efcc22dbd1
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2023-03-18 18:50:21 +0800

    Optimize theme switching

--- Dev/Typedown.Editor/src/services/theme.ts
+++ Dev/Typedown.Editor/src/services/theme.ts
@@ -8,47 +8,26 @@ remote.getCurrentTheme().then(arg => {
     setTimeout(() => remote.contentLoaded(), 0);
 })
 
+function getorCreateStyle(id: string) {
+    let style = document.getElementById(id) as HTMLLinkElement;
+    if (!style) {
+        style = document.createElement("link");
+        style.rel = "stylesheet";
+        style.id = id;
+        document.head.appendChild(style)
+    }
+    return style;
+}
+
 function onThemeChanged({ theme, accentColor, background }: any) {
-    
-    let editorStyleDocument = document.getElementById("link_style_editor") as HTMLLinkElement;
-    let prismjsStyleDocument = document.getElementById("link_style_prismjs") as HTMLLinkElement;
-    let codemirrorStyleDocument = document.getElementById("link_style_codemirror") as HTMLLinkElement;
-
-    if (!editorStyleDocument) {
-        editorStyleDocument = document.createElement("link");
-        editorStyleDocument.rel = "stylesheet";
-        editorStyleDocument.id = "link_style_editor";
-        document.head.appendChild(editorStyleDocument)
-    }
-
-    if (!prismjsStyleDocument) {
-        prismjsStyleDocument = document.createElement("link");
-        prismjsStyleDocument.rel = "stylesheet";
-        prismjsStyleDocument.id = "link_style_prismjs";
-        document.head.appendChild(prismjsStyleDocument)
-    }
-
-    if (!codemirrorStyleDocument) {
-        codemirrorStyleDocument = document.createElement("link");
-        codemirrorStyleDocument.rel = "stylesheet";
-        codemirrorStyleDocument.id = "link_style_codemirror";
-        document.head.appendChild(codemirrorStyleDocument)
-    }
+    const editorStyleDocument = getorCreateStyle("link_style_editor");
+    const prismjsStyleDocument = getorCreateStyle("link_style_prismjs");
+    const codemirrorStyleDocument = getorCreateStyle("link_style_codemirror");
 
     theme = theme?.toLowerCase()
-
-    switch (theme) {
-        case "light":
-            editorStyleDocument.href = "theme/editor/light.theme.css"
-            prismjsStyleDocument.href = "theme/prismjs/light.theme.css"
-            codemirrorStyleDocument.href = "theme/codemirror/light.theme.css"
-            break;
-        case "dark":
-            editorStyleDocument.href = "theme/editor/dark.theme.css"
-            prismjsStyleDocument.href = "theme/prismjs/dark.theme.css"
-            codemirrorStyleDocument.href = "theme/codemirror/dark.theme.css"
-            break;
-    }
+    editorStyleDocument.href = `theme/editor/${theme}.theme.css`
+    prismjsStyleDocument.href = `theme/prismjs/${theme}.theme.css`
+    codemirrorStyleDocument.href = `theme/codemirror/${theme}.theme.css`
 
     const themeColorAlphas = [10, 20, 30, 40, 50, 60, 70, 80, 90]
     const { r, g, b, a } = accentColor
